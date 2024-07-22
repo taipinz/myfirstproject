8c6dc34d0b8f4d15a7421acbc3e4284df2ab2328
taipinz <c1422884226@163.com>
2024-07-22 10:26:05 +0800

    测试文件
    啦啦啦

---
+++ c4.3.py
@@ -0,0 +1,16 @@
+# for value in range(5):
+#  print(value)
+# numbers = list(range(1,5))
+# print(numbers)
+
+
+
+# squares = []
+# for sz in range(1,11) :
+#  square = sz ** 2
+#  squares.append(square)
+
+# print(squares)
+
+numbers = [number + 2 for number in range(6)]
+print(numbers)
